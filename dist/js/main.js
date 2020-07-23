ee62dcaf5f5ec2a3e61518891ca8c7ba53b262cf
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-07-22 22:28:02 -0700

    Doing my best to spice up the background. Removed the who is this for section/spinner and made the background of the sample purple to mimick stripe's cool demo page. UI design is hard...

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+/*const uses = ["Journalists", "Bloggers", "Podcasters", "Lecturers", "Interviewers", "Sermoners", "Investigators", "Laywers"];
+
+let useIndex = 0;
+
+function incrementUse(n) {
+    useIndex += n;
+    if(useIndex >= uses.length) {
+        useIndex = 0;
+    } else if(useIndex < 0) {
+        useIndex = uses.length - 1;
+    }
+    let use = document.getElementsByClassName("use");
+    use[0].innerHTML = uses[useIndex];
+}*/
